8249c259c319ef6c94666e9e9f16dd160c4f65ea
Muslim <dmooez.dev@gmail.com>
2024-09-26 15:04:33 +0700

    Add Auth

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 // ADMIN
 use App\Http\Controllers\Admin\DashboardController;
@@ -13,17 +13,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::get('/login', function () {
+    return view('auth.login');
 });
 
-Route::middleware('auth')->prefix('admin')->name('admin.')->group(function () {
+Route::post('/login', [
+    LoginController::class, 'authenticate'
+])->name('login');
+
+Route::post('/logout', [
+    LoginController::class, 'logout'
+])->name('logout');
+
+Route::middleware(['auth'])->prefix('admin')->name('admin.')->group(function () {
     // DASHBOARD
     Route::get('/dashboard', [
         DashboardController::class, 'index'
@@ -98,5 +100,3 @@ Route::middleware('auth')->prefix('admin')->name('admin.')->group(function () {
         TransaksiController::class, 'export'
     ])->name('export_transaksi');
 });
-
-require __DIR__.'/auth.php';
