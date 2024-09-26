aebd28e2137f725429e734f4cc4492447464e03a
Muslim <dmooez.dev@gmail.com>
2024-09-26 15:23:04 +0700

    Add Amcharts to Plugins

---
+++ resources/views/plugins/amcharts_js.blade.php
@@ -0,0 +1,5 @@
+<!-- Resources -->
+<script src="{{ asset('assets/amcharts/index.js') }}"></script>
+<script src="{{ asset('assets/amcharts/xy.js') }}"></script>
+<script src="{{ asset('assets/amcharts/percent.js') }}"></script>
+<script src="{{ asset('assets/amcharts/themes/Animated.js') }}"></script>
